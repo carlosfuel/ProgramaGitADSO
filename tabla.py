f146034f830e545e36b1df896e057cb91d3e35e8
Carlos Alberto Fuel <cafuel@udenar.edu.co>
2025-08-04 19:55:47 -0500

    se crea el archivo Python tabla.py

---
+++ tabla.py
@@ -0,0 +1,20 @@
+class TablaMultiplicar:
+    def __init__(self,numero):
+        self.numero = numero
+        
+    def imprimir_tabla(self):
+        print
+        for i in range(1, 11):
+            resultado = self.numero * i
+            print(f"{self.numero} x {i} = {resultado}")
+
+def main():
+    try:
+        numero = int(input("Ingrese un número para mostrar su tabla de multiplicar: "))
+        tabla = TablaMultiplicar(numero)
+        tabla.imprimir_tabla()
+    except ValueError:
+        print("Por favor, ingrese un número válido.")
+
+if __name__ == "__main__":
+    main()
